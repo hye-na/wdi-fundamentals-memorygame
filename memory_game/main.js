3a4d90805a4fdf1fa3d1337a640150bdd4e0974f
Hannah Park <hye.na.pa@gmail.com>
2018-09-10 01:19:03 -0700

    created game logic

--- memory_game/main.js
+++ memory_game/main.js
@@ -1,27 +1,112 @@
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var cards = [
+{
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+},
+{
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+},
+{
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+ },
+{
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+},
+{
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+},
+{
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+},
+{
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+},
+{
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+}
+];
 
-var card = ["queen", "queen", "king", "king"];
 
-cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[1];
 
+var cardsInPlay = [];
+cardsInPlay.push(cards[cardId].rank);
 
-//Create a variable cardOne.
-//Store the first item in the cards array in the variable cardOne.
-
-var cardOne = cardsInPlay.push(cards[0]) {
-    console.log("User flipped queen")
-};
-
-var cardTwo = cardsInPlay.push(cards[2]) {
-    console.log("User flipped king")
-};
-
+   
 var cardsMatch = function () {
-    if (cardsInPlay.length === 2 && cardOne === cardTwo);
-    } else alert("Sorry, try again!"); {
+    if (cardsInPlay.length === 2 && cardOne === cardTwo){
+        return alert("You found a match!")
+    } else { 
+        return alert("Sorry, try again!")
     }
-alert("You found a match!");
 };
 
 
+
+
+var checkForMatch = function() {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        console.log("You found a match!")
+    } else {
+        console.log("Sorry, try again.")
+        }
+};
+
+var flipCard = function() {
+    var cardId = this.getAttribute('data-id');
+    this.setAttribute('src', cards[cardId].cardImage);
+    
+/*    
+    if () 
+    {
+    ;
+    }
+    else (){
+        ;
+    }   
+};
+*/
+
+  
+checkForMatch();
+
+
+
+console.log("User flipped " + cards[cards[cardId].rank]);
+console.log("User flipped " + cards[cards[cardId].rank]);
+    
+console.log(cards[cards[cardId].cardImage]);
+console.log(cards[cards[cardId].suit]);
+
+
+
+
+var createBoard = function() {
+    
+    for (var i = 0; i < cards.length; i++) {
+        var createElement = document.createElement('img');
+        var cardElement = [
+            cardElement.setAttribute('src', 'images/back.png'),
+            cardElement.setAttribute('data-id', i),
+            cardElement.addEventListener('click', flipCard);
+            document.getElementById('game-board').appendChild(cardElement)],
+    }
+};
+
+createBoard();
