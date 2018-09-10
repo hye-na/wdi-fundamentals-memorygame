3ce969159b1708c66d4267af2d37049627d28e95
Hannah Park <hye.na.pa@gmail.com>
2018-09-09 22:16:39 -0700

    created game logic

---
+++ memory_game/main.js
@@ -0,0 +1,27 @@
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
+
+var card = ["queen", "queen", "king", "king"];
+
+cardsInPlay = [];
+
+
+//Create a variable cardOne.
+//Store the first item in the cards array in the variable cardOne.
+
+var cardOne = cardsInPlay.push(cards[0]) {
+    console.log("User flipped queen")
+};
+
+var cardTwo = cardsInPlay.push(cards[2]) {
+    console.log("User flipped king")
+};
+
+var cardsMatch = function () {
+    if (cardsInPlay.length === 2 && cardOne === cardTwo);
+    } else alert("Sorry, try again!"); {
+    }
+alert("You found a match!");
+};
+
+
